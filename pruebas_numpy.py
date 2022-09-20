5a23a7f691a843e279592c6dbb50517300b89dde
gdsad <gonzalosad@gmail.com>
2022-09-20 15:49:09 -0300

    Second Commit

--- pruebas_numpy.py
+++ pruebas_numpy.py
@@ -13,3 +13,22 @@ type(aa)
 aa.shape
 aa.dtype
 
+# ----
+x = np.array([[1,2,3],[4,5,6],[7,8,9]],dtype=np.float64)
+x
+type(x)
+x.dtype
+x.shape
+x[1]
+x[:,0]
+
+
+#  ------------------------
+w = np.linspace(0,10,25)
+w
+w.shape
+len(w)
+w.dim()
+w2 = w[w<5]
+
+w3 = 22
